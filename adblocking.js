850aa6f2d5d2082c3c2436ea78590ef83d40f401
merfemor <efremov.roman98@gmail.com>
2017-09-13 12:09:12 +0300

    async donate check

--- adblocking.js
+++ adblocking.js
@@ -1,14 +1,14 @@
 document.getElementById('turn-off-ads').onclick = function() {
-    setTimeout(function(){
-        var status = is_donated();
-        if(status === true) {
-            hide_ads(true);
-        } else if (status === false) {
-            alert('You have no power here.\nDonate, pls\n');
-        } else if(status === null) {
-            alert('Failed to load the donate information.\nPlease, check yout internet connection and try again.');
-        }
-    }, 1);
+	var status = is_donated(function(status) {
+		console.log("is_donated status: " + status);
+		if(status === true) {
+			hide_ads(true);
+		} else if (status === false) {
+			alert('You have no power here.\nDonate, pls\n');
+		} else if(status == null) {
+			alert('Failed to load the donate information.\nPlease, check yout internet connection and try again.');
+		}
+	});
 };
 
 function hide_ads(alert_message = false) {
@@ -19,27 +19,45 @@ function hide_ads(alert_message = false) {
     }
 }
 
-var GET_INFO_URL = 'https://sheets.googleapis.com/v4/spreadsheets/1Hy3Nqjxm92540mfwli7DE3U2UBiP8NVrii7CUoa02xU/values/1!A2?key=AIzaSyD_Opt6qNmkiaFw_4NZvLrSr4n3WUZ8ryE';
-
-function is_donated() {
-    var content = get_page_content(GET_INFO_URL);
-    if(content == null) {
-        return null;
-    }
-	return content.search("FALSE") === -1;
+function show_ads() {
+	document.getElementById("ad-unit-l").style.display="block";
+	document.getElementById("ad-unit-r").style.display="block";
 }
 
-function get_page_content(url)
-{
-    xmlhttp=new XMLHttpRequest();
-    xmlhttp.open("GET", url, false );
+var GET_INFO_URL = 'https://sheets.googleapis.com/v4/spreadsheets/1Hy3Nqjxm92540mfwli7DE3U2UBiP8NVrii7CUoa02xU/values/1!A2?key=AIzaSyD_Opt6qNmkiaFw_4NZvLrSr4n3WUZ8ryE';
+
+function is_donated(func) {
+    get_page_content(GET_INFO_URL, function(content) {
+		if(content != null) {
+			var ret = content.search("FALSE") === -1;
+		}
+		func(ret);
+	});
+}
+
+function get_page_content(url, func) {
+	if(window.XMLHttpRequest){  
+		xmlhttp=new XMLHttpRequest();
+	} else if(window.ActiveXObject){  
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");  
+	}  
     try {
-        xmlhttp.send(null);
+		xmlhttp.onreadystatechange=function(){
+			if (xmlhttp.readyState==4) {
+				if(xmlhttp.status!=200) {
+					console.error('Bad xmlhttp.status = ' + xmlhttp.status);
+					func(null);
+				} else {
+					func(xmlhttp.responseText);
+				}
+			}
+		};  
+		xmlhttp.open("GET", url, true);
+        xmlhttp.send();
     } catch (e) {
+		console.error('Exception: ' + e);
         return null;
     }
-    if (xmlhttp.status==200) {
-        return xmlhttp.responseText;
-    }
+    
     return null;
 }
