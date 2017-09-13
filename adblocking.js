da40fc6e426351fb23330e451b7b5b1f3030db7b
merfemor <efremov.roman98@gmail.com>
2017-09-13 06:03:09 +0300

    checking donation onload

--- adblocking.js
+++ adblocking.js
@@ -1,20 +1,29 @@
 document.getElementById('turn-off-ads').onclick = function() {
-    var status = is_donated();
-	if(status === true) {
-	    alert('Thanks for donating!');
-		document.getElementById("ad-unit-r").style.display = "none";
-		document.getElementById("ad-unit-l").style.display = "none";
-	} else if (status === false) {
-		alert('You have no power here.\nDonate, pls\n');
-	}
+    setTimeout(function(){
+        var status = is_donated();
+        if(status === true) {
+            hide_ads(true);
+        } else if (status === false) {
+            alert('You have no power here.\nDonate, pls\n');
+        } else if(status === null) {
+            alert('Failed to load the donate information.\nPlease, check yout internet connection and try again.');
+        }
+    }, 1);
 };
+
+function hide_ads(alert_message = false) {
+    document.getElementById("ad-unit-r").style.display = "none";
+    document.getElementById("ad-unit-l").style.display = "none";
+    if(alert_message) {
+        alert('Thanks for donating!')
+    }
+}
 
 var GET_INFO_URL = 'https://sheets.googleapis.com/v4/spreadsheets/1Hy3Nqjxm92540mfwli7DE3U2UBiP8NVrii7CUoa02xU/values/1!A2?key=AIzaSyD_Opt6qNmkiaFw_4NZvLrSr4n3WUZ8ryE';
 
 function is_donated() {
     var content = get_page_content(GET_INFO_URL);
     if(content == null) {
-        alert('Failed to load the donate information.\nPlease, check yout internet connection and try again.');
         return null;
     }
 	return content.search("FALSE") === -1;
