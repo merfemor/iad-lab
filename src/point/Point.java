b24f16a2e4c223f06e0c309f2e77ecdcb4b43644
merfemor <efremov.roman98@gmail.com>
2017-10-23 04:12:53 +0300

    Hibernate Idea Framework Support

--- src/point/Point.java
+++ src/point/Point.java
@@ -1,16 +1,52 @@
 package point;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Point {
-    public final double x;
-    public final double y;
-    public final double radius;
-    public final boolean isInRegion;
+    public double x;
+    public double y;
+    public double radius;
+    public boolean isInRegion;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
 
     public Point(double x, double y, double radius) {
         this.x = x;
         this.y = y;
         this.radius = radius;
         this.isInRegion = isInRegion();
+    }
+
+    public Point() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
     }
 
     private boolean isInRegion() {
