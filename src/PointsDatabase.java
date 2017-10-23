2445019a7df9540b041dc0edb2ce03fcb428bc8c
merfemor <efremov.roman98@gmail.com>
2017-10-23 03:46:15 +0300

    database class and hibernate config

---
+++ src/PointsDatabase.java
@@ -0,0 +1,14 @@
+import point.Point;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class PointsDatabase {
+    public void store(Point point) {
+
+    }
+
+    public List<Point> getPoints() {
+        return new LinkedList<>();
+    }
+}
