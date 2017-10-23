1179ecce38e16673fbd1c00c301988ac05c04433
merfemor <efremov.roman98@gmail.com>
2017-10-23 04:41:33 +0300

    implemented store/get point methods

--- src/PointsDatabase.java
+++ src/PointsDatabase.java
@@ -1,14 +1,27 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import point.Point;
 
-import java.util.LinkedList;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class PointsDatabase {
-    public void store(Point point) {
+    private final SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 
+    public void store(Point point) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(point);
+        session.getTransaction().commit();
+        session.close();
     }
 
     public List<Point> getPoints() {
-        return new LinkedList<>();
+        Session session = sessionFactory.openSession();
+        TypedQuery<Point> query = session.createQuery("from Point", Point.class);
+        List<Point> points = query.getResultList();
+        session.close();
+        return points;
     }
 }
