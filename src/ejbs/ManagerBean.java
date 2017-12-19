aaafc139c8713a4b043f3f465bef714ae67bed86
Your Name <you@example.com>
2017-12-20 01:24:49 +0300

    to lab4

--- src/ejbs/ManagerBean.java
+++ src/ejbs/ManagerBean.java
@@ -24,18 +24,26 @@ public class ManagerBean {
     @GET
     @Produces("application/json")
     @Path("/users/{owner_id}/points")
-    public String getPoints(@PathParam("owner_id") String id) {
+    public String getPoints(@PathParam("owner_id") String id, @QueryParam("password") String password) {
         Session session = sessionFactory.openSession();
-        TypedQuery<Point> query = session.createQuery("from Point where owner.id=:paramName", Point.class);
-        query.setParameter("paramName", Long.valueOf(id));
-        List<Point> points = query.getResultList();
+        TypedQuery<Point> pointQuery = session.createQuery("from Point where owner.id=:paramName", Point.class);
+        pointQuery.setParameter("paramName", Long.valueOf(id));
+        List<Point> points = pointQuery.getResultList();
         if (points.size() == 0) {
             ExceptMessage mes = new ExceptMessage("No such items");
             return gson.toJson(mes);
         }
-        session.close();
 
-        return gson.toJson(points);
+        TypedQuery<User> userQuery = session.createQuery("from User where id=:paramName", User.class);
+        userQuery.setParameter("paramName", Long.valueOf(id));
+        List<User> users = userQuery.getResultList();
+        User user = users.get(0);
+        session.close();
+        if (user.getPassword().equals(String.valueOf(password.hashCode()))) {
+            return gson.toJson(points);
+        } else {
+            return gson.toJson("Incorrect password");
+        }
     }
 
     @POST
@@ -57,7 +65,7 @@ public class ManagerBean {
     @POST
     @Produces("application/json")
     @Path("/users/{owner_id}/points")
-    public String sendPoint(@PathParam("owner_id") String id, String pointJson) {
+    public String sendPoint(@PathParam("owner_id") String id, String pointJson, @QueryParam("password") String password) {
         Session session = sessionFactory.openSession();
         TypedQuery<User> query = session.createQuery("from User where id=:paramName", User.class);
         query.setParameter("paramName", Long.valueOf(id));
@@ -67,16 +75,19 @@ public class ManagerBean {
             ExceptMessage mes = new ExceptMessage("No such items");
             return gson.toJson(mes);
         }
+        if (user.getPassword().equals(String.valueOf(password.hashCode()))) {
+            Point point = gson.fromJson(pointJson, Point.class);
+            point.setOwner(user);
 
-        Point point = gson.fromJson(pointJson, Point.class);
-        point.setOwner(user);
+            session.beginTransaction();
+            session.save(point);
+            session.getTransaction().commit();
+            session.close();
 
-        session.beginTransaction();
-        session.save(point);
-        session.getTransaction().commit();
-        session.close();
-
-        return gson.toJson(point);
+            return gson.toJson(point);
+        } else {
+            return gson.toJson("Incorrect password");
+        }
     }
 
     @GET
