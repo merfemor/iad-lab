8f1c6f9a6ef5af2fa66ef482fc9fbaffa442d482
Your Name <you@example.com>
2017-12-19 06:08:00 +0300

    to lab4

--- src/ejbs/ManagerBeanInterface.java
+++ src/ejbs/ManagerBeanInterface.java
@@ -1,35 +1,32 @@
 package ejbs;
 
-import graphics.Point;
-import graphics.User;
-
 import javax.ejb.Local;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import java.util.List;
+import javax.ws.rs.Produces;
 
-@Path("/")
+@Path("/hello")
 @Local
 public interface ManagerBeanInterface {
+//
+//    @GET
+////    @Path("points")
+//    public List<Point> getPoints(User user);
+//
+//    @POST
+////    @Path("user/id")
+//    public void sendUser(User user);
+//
+//    @POST
+////    @Path("user/id")
+//    public void sendPoint(Point point);
+//
+//    @GET
+////    @Path("user")
+//    public User getUser(int id);
 
     @GET
-    @Path("points")
-    public List<Point> getPoints(User user);
-
-    @POST
-    @Path("user/id")
-    public void sendUser(User user);
-
-    @POST
-    @Path("user/id")
-    public void sendPoint(Point point);
-
-    @GET
-    @Path("user")
-    public User getUser(int id);
-
-    @GET
-    @Path("hello")
+    @Path("/hello")
+    @Produces("text/plain")
     public String hello();
 }
