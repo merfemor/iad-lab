95710b819c140c20696e5ce9c54cc8982c69c394
Your Name <you@example.com>
2017-12-19 06:08:00 +0300

    to lab4

--- src/ejbs/ManagerBean.java
+++ src/ejbs/ManagerBean.java
@@ -1,63 +1,56 @@
 package ejbs;
 
-import graphics.Point;
-import graphics.User;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-
 import javax.ejb.Stateless;
-import javax.persistence.TypedQuery;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import java.util.List;
+import javax.ws.rs.Produces;
 
 @Stateless
-@Path("/")
+@Path("/hello")
 public class ManagerBean implements ManagerBeanInterface {
-    private final SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-
-    @Override
-    public List<Point> getPoints(User user) {
-        Session session = sessionFactory.openSession();
-        TypedQuery<Point> query = session.createQuery("from Point where owner=:paramName", Point.class);
-        query.setParameter("paramName", user);
-        List<Point> points = query.getResultList();
-        session.close();
-        return points;
-    }
-
-    @Override
-    public void sendUser(User user) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.save(user);
-        session.getTransaction().commit();
-        session.close();
-    }
-
-    @Override
-    public void sendPoint(Point point) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.save(point);
-        session.getTransaction().commit();
-        session.close();
-    }
-
-    @Override
-    public User getUser(int id) {
-        Session session = sessionFactory.openSession();
-        TypedQuery<User> query = session.createQuery("from User where id=:paramName", User.class);
-        query.setParameter("paramName", id);
-        List<User> users = query.getResultList();
-        User user = users.get(0);
-        session.close();
-        return user;
-    }
+//    private final SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+//
+//    @Override
+//    public List<Point> getPoints(User user) {
+//        Session session = sessionFactory.openSession();
+//        TypedQuery<Point> query = session.createQuery("from Point where owner=:paramName", Point.class);
+//        query.setParameter("paramName", user);
+//        List<Point> points = query.getResultList();
+//        session.close();
+//        return points;
+//    }
+//
+//    @Override
+//    public void sendUser(User user) {
+//        Session session = sessionFactory.openSession();
+//        session.beginTransaction();
+//        session.save(user);
+//        session.getTransaction().commit();
+//        session.close();
+//    }
+//
+//    @Override
+//    public void sendPoint(Point point) {
+//        Session session = sessionFactory.openSession();
+//        session.beginTransaction();
+//        session.save(point);
+//        session.getTransaction().commit();
+//        session.close();
+//    }
+//
+//    @Override
+//    public User getUser(int id) {
+//        Session session = sessionFactory.openSession();
+//        TypedQuery<User> query = session.createQuery("from User where id=:paramName", User.class);
+//        query.setParameter("paramName", id);
+//        List<User> users = query.getResultList();
+//        User user = users.get(0);
+//        session.close();
+//        return user;
+//    }
 
     @GET
-    @Path("hello")
+    @Produces("text/plain")
     @Override
     public String hello() {
         return "Hello, HUI";
