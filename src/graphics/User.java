26a6e74a0054eac227beb110e1c491cdccfb8cf5
Your Name <you@example.com>
2017-12-19 07:01:04 +0300

    to lab4

--- src/graphics/User.java
+++ src/graphics/User.java
@@ -4,19 +4,15 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.ws.rs.FormParam;
 
 @Entity
 public class User {
     @Id
     @GeneratedValue
-    @FormParam("id")
     private Long id;
     @Column
-    @FormParam("login")
     private String login;
     @Column
-    @FormParam("password_hash")
     private String password_hash;
 
     public User(String login, String password_hash) {
